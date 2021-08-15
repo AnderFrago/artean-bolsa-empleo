c198390f491c24595c07e7d9b7bdeaa433424505
ander frago landa <ander_frago@cuatrovientos.org>
2021-08-15 06:46:35 +0200

    update Migratinos Version20210721143044

--- backend/migrations/Version20210721143044.php
+++ backend/migrations/Version20210721143044.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210721143017 extends AbstractMigration
+final class Version20210721143044 extends AbstractMigration
 {
     public function getDescription(): string
     {
